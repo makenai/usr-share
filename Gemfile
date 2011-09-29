60cecf45375e17e065af1148ff32c1d20e79bd0e
Pawel Szymczykowski <makenai@gmail.com>
2011-09-28 18:28:36 -0700

    Created /usr/share base app

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'rghost'
+gem 'rghost_barcode'
+gem 'omniauth'
+gem 'devise'
+
+group :development do
+  gem "nifty-generators"
+  gem 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usrshare::Application.config.secret_token = 'f6fa9b65106c01161977db53ea5051fc4cfa5e985e536f0462d8fc031d2c818c7a613b8f31ea5b28dce56bd0ec05fb3c916360c8a87e26d078493f832d345a04'

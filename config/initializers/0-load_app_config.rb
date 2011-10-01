c468e04d76035ef4f099d7685bf6731d10c9b331
Pawel Szymczykowski <makenai@gmail.com>
2011-10-01 11:55:39 -0500

    More heroku config hackery

--- config/initializers/0-load_app_config.rb
+++ config/initializers/0-load_app_config.rb
@@ -1,2 +1,2 @@
-raw_config = File.read("#{Rails.root}/config/app_config.yml") rescue {}
-APP_CONFIG = YAML.load(raw_config)[Rails.env].merge(ENV).symbolize_keys
+raw_config = File.read("#{Rails.root}/config/app_config.yml") rescue nil
+APP_CONFIG = ( raw_config ? YAML.load(raw_config)[Rails.env] : {} ).merge(ENV).symbolize_keys

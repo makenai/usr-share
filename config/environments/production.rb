bdfdc14b51aabe34b9256c266ee0daa1be64147d
Pawel Szymczykowski <makenai@gmail.com>
2011-11-16 10:12:47 -0800

    Changing to custom domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,5 @@ Usrshare::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'usrshare.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'usrlib.org' }
 end
